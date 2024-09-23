8314aa1f04925cf3dc9a4c7d5c1d2d728f892a4a
timecra2 <79523361+timecra2@users.noreply.github.com>
2024-09-23 13:11:33 +0900

    explanation added

--- assignment 01/main.py
+++ assignment 01/main.py
@@ -1,1 +1,4 @@
+#23101190 Shin Yuhee
+#Python Basic Print Test
+
 print("Hello,World!")
--- assignment 01/testimport.py
+++ assignment 01/testimport.py
@@ -1,3 +1,8 @@
+#23101190 Shin YuHee
+#python module importing test
+#numpy, opencv, matplotlib
+
+
 import numpy
 import cv2
 import ipdb 
